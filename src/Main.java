ff270ee36b7f8452a23f03a9e03ecdf9ca9875ee
Mauricio <stb539@gmail.com>
2024-04-29 17:18:41 -0400

    jar setTime

--- src/Main.java
+++ src/Main.java
@@ -19,61 +19,59 @@ public class Main {
 
             System.out.println("Ingrese opcion");
             try {
-            int opcion = leer.nextInt();
+                int opcion = leer.nextInt();
 
-            
+                // int opcion = leer.nextInt();
 
-            // int opcion = leer.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    try {
-                        for (int i = 0; i < 5; i++) { // 5 segundos
-                            Thread.sleep(1000);
-                            System.out.println("Sleep " + i);
+                switch (opcion) {
+                    case 1:
+                        try {
+                            for (int i = 0; i < 5; i++) { // 5 segundos como setTime
+                                Thread.sleep(1000);
+                                System.out.println("Sleep " + i);
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
                         }
-                    } catch (Exception e) {
-                        System.out.println(e);
-                    }
-                    System.out.println("ingrese primer numero");
-                    num1 = leer.nextInt();
-                    System.out.println("ingrese segundo numero");
-                    num2 = leer.nextInt();
-                    Suma suma = new Suma(num1, num2);
-                    System.out.println("La suma es: " + suma.mostrarResultado());
-                    break;
-                case 2:
-                    System.out.println("ingrese primer numero");
-                    num1 = leer.nextInt();
-                    System.out.println("ingrese segundo numero");
-                    num2 = leer.nextInt();
-                    Resta resta = new Resta(num1, num2);
-                    System.out.println("La resta es: " + resta.mostrarResultado());
-                    break;
-                case 3:
-                    System.out.println("ingrese primer numero");
-                    num1 = leer.nextInt();
-                    System.out.println("ingrese segundo numero");
-                    num2 = leer.nextInt();
-                    Division dividir = new Division(num1, num2);
-                    System.out.println("La division es: " + dividir.mostrarResultado());
-                    break;
-                case 4:
-                    System.out.println("ingrese primer numero");
-                    num1 = leer.nextInt();
-                    System.out.println("ingrese segundo numero");
-                    num2 = leer.nextInt();
-                    Multiplicacion multiplicar = new Multiplicacion(num1, num2);
-                    System.out.println("La multiplicacion es: " + multiplicar.mostrarResultado());
-                    break;
-                default:
-                    System.out.println("opcion no existe");
-                    break;
-            }
+                        System.out.println("ingrese primer numero");
+                        num1 = leer.nextInt();
+                        System.out.println("ingrese segundo numero");
+                        num2 = leer.nextInt();
+                        Suma suma = new Suma(num1, num2);
+                        System.out.println("La suma es: " + suma.mostrarResultado());
+                        break;
+                    case 2:
+                        System.out.println("ingrese primer numero");
+                        num1 = leer.nextInt();
+                        System.out.println("ingrese segundo numero");
+                        num2 = leer.nextInt();
+                        Resta resta = new Resta(num1, num2);
+                        System.out.println("La resta es: " + resta.mostrarResultado());
+                        break;
+                    case 3:
+                        System.out.println("ingrese primer numero");
+                        num1 = leer.nextInt();
+                        System.out.println("ingrese segundo numero");
+                        num2 = leer.nextInt();
+                        Division dividir = new Division(num1, num2);
+                        System.out.println("La division es: " + dividir.mostrarResultado());
+                        break;
+                    case 4:
+                        System.out.println("ingrese primer numero");
+                        num1 = leer.nextInt();
+                        System.out.println("ingrese segundo numero");
+                        num2 = leer.nextInt();
+                        Multiplicacion multiplicar = new Multiplicacion(num1, num2);
+                        System.out.println("La multiplicacion es: " + multiplicar.mostrarResultado());
+                        break;
+                    default:
+                        System.out.println("opcion no existe");
+                        break;
+                }
 
             } catch (Exception e) {
-            System.out.println(" ingrese solo numero de opcion");
-        }
+                System.out.println(" ingrese solo numero de opcion");
+            }
 
             leer.nextLine();
             System.out.println("Si desa continuar con otra opercacion ingrese s, sino Enter u otro caracter");
@@ -86,9 +84,11 @@ public class Main {
             }
         }
         leer.close();
-        do {
-
-        } while(opcion !=0);
+        /*
+         * do {
+         * 
+         * } while(opcion !=0);
+         */
 
     }
 
